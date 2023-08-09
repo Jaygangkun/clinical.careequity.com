9687e98bd82a2d92450c531ceaeadcb777434815
Jay <34517373+Jaygangkun@users.noreply.github.com>
2023-08-08 23:03:45 -0400

    Update database.php to hide information

--- application/config/database.php
+++ application/config/database.php
@@ -79,9 +79,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'evan7ce_clinical_rss',
-	'password' => 'km_6$zPR==dP',
-	'database' => 'evan7ce_clinical_rss',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -98,26 +98,3 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-/*
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'evan7ce_clinical_rss',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-*/
